eedffc2b91dd9f1aac07714d4368fe33b6a8e107
qkdjhw <114429818+qkdjhw@users.noreply.github.com>
2022-09-28 12:53:48 -0400

    Add files via upload

---
+++ Decisionmaking1.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.decisionmaking1;
+
+/**
+ *
+ * @author user
+ */
+public class Decisionmaking1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        /* if(10>20)
+        {
+            System.out.println ("10 is greater than 20");
+        }
+        else
+        {
+            System.out.println ("20 is greater than 10");
+        } */
+        
+        /* if (3==6)
+        {
+            System.out.println ("3 is equal to 6");
+        }
+        else
+        {
+            System.out.println ("3 is not equal to 6");
+        }
+        if (4!=2)
+        {
+            System.out.println ("4 is not equal to 2");
+        }
+        else
+        {
+            System.out.println ("4 is equal to 2");
+        }*/
+        /*char a=1, b=2;
+        if (a==b)
+        {
+            System.out.println ("a is equal to b");
+        }
+        else 
+        {
+            System.out.println ("a is not equal to b");
+        } 
+        char c1='a', c2='A';
+        if (c1==c2)
+        {
+            System.out.println ("c1 is equal to c2");
+        }
+        else
+        {
+            System.out.println ("c1 is not equal to c2");
+        } */
+        if (i==10 || i<15){
+        {
+        if (1<15)
+            System.out.println ("i is smaller than 15 too");
+        if (1<12)
+            System.out.println ("i is smaller than 12 too");
+        }
+        else
+        {
+            System.out.println("i is greater than 15");
+        }
+        }
+    }
+        
+    }
+}
